2e5ce0b79d2cbaaaadd982b6767c48def2e1104f
Дмитрий Гордеев <netimen2@yandex.ru>
2025-02-17 18:05:38 +0300

    removed unused

--- feature/list/build.gradle.kts
+++ feature/list/build.gradle.kts
@@ -17,15 +17,6 @@ android {
         consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
