47855653bec93eefa0f9a98a030c2f16be052ac2
Дмитрий Гордеев <netimen2@yandex.ru>
2025-03-07 11:35:46 +0300

    support auto audiofocus

--- app/src/main/java/yandex/practicum/middle/android/ui/PlayerScreen.kt
+++ app/src/main/java/yandex/practicum/middle/android/ui/PlayerScreen.kt
@@ -1,8 +1,5 @@
 package yandex.practicum.middle.android.ui
 
-import android.content.Context
-import android.media.AudioFocusRequest
-import android.media.AudioManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,23 +10,18 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
-import androidx.media3.common.Player.Listener
 import androidx.media3.ui.PlayerView
 import kotlinx.coroutines.launch
 
 @Composable
 fun PlayerScreen(player: Player, mediaItem: MediaItem) {
-    val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(mediaItem) {
         coroutineScope.launch {
-            setupAudioFocus(context, player)
             player.setMediaItem(mediaItem)
             player.prepare()
             player.playWhenReady = true
@@ -52,30 +44,4 @@ fun PlayerScreen(player: Player, mediaItem: MediaItem) {
             }
         )
     }
-}
-
-private fun setupAudioFocus(context: Context, player: Player) {
-    val audioManager = getSystemService(context, AudioManager::class.java)!!
-
-    val focusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
-        .setOnAudioFocusChangeListener { focusChange ->
-            when (focusChange) {
-                AudioManager.AUDIOFOCUS_LOSS -> player.stop()
-                AudioManager.AUDIOFOCUS_LOSS_TRANSIENT -> player.pause()
-                AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK -> player.volume = 0.2f
-                AudioManager.AUDIOFOCUS_GAIN -> {
-                    player.play()
-                    player.volume = 1f
-                }
-            }
-        }
-        .build()
-
-    player.addListener(object : Listener {
-        override fun onPlaybackStateChanged(playbackState: Int) {
-            if (playbackState == Player.STATE_ENDED)
-                audioManager.abandonAudioFocusRequest(focusRequest)
-        }
-    })
-    audioManager.requestAudioFocus(focusRequest)
 }
